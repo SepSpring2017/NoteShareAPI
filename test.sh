fe98e26d589024f118120f1a24ff34ee2430055b
Michael Codner <mhcodner@gmail.com>
2017-09-07 14:28:40 +1000

    Test error messages for registration

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-ACCESS_TOKEN="$(curl -v -H "Content-Type: application/x-www-form-urlencoded" -X POST -d 'grant_type=password&username=test@test.com&password=J8cG!FjD' http://localhost:5000/connect/token | jq -r '.access_token')"
+# Check that we receive a proper error message for the user registration
+curl -v -H "Content-Type: application/json" -d '{Email:"testno",Password:"notlong"}' http://localhost:5000/api/Users
 
-echo $ACCESS_TOKEN
+# Generate an access token
+ACCESS_TOKEN="$(curl -v -H "Content-Type: application/x-www-form-urlencoded" -d 'grant_type=password&username=test@test.com&password=J8cG!FjD' http://localhost:5000/connect/token | jq -r '.access_token')"
 
-# Passing the ACCESS_TOKEN as a Authorization header and quering the list of groceries
+# Passing the ACCESS_TOKEN as an Authorization header and quering the list of users
 curl -v -H "Authorization: Bearer $ACCESS_TOKEN" http://localhost:5000/api/users
